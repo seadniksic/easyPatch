f86d92495340b40ff2cda256c5830736e65445e4
Sead Niksic <seaddniksic@gmail.com>
2023-09-25 02:20:51 -0400

    introducing support for hdf5

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
+## easyPatch
+-------------
+** this repo is currently under construction and prone to change
 
+### About
+
+This library allows you to easily patch histopathological images (whole slide images [WSI]).  Features include
+ - generating patches using a desired zoom level and overlap
+ - generating a binary mask from ground truth XML annotations
+ - recombining patches into original image dimension 
+
+easyPatch stores the generated patches in HDF5 files on disk as to not overwhelm RAM usage, as a single WSI can be constituted of thousands of patches depending on the zoom level. 
+
+### Usage
+
+This library is not registered on the PyPI as of yet. To use it please clone the source code and follow directions below.
+All commands referenced below assume the user to be in the top level of the repo.
+
+``` import create_patches.patch as patch```
+
+To generate patches of multiple WSI images
+
+``` patch.patch_images_without_mask(INPUT_DIR, OUTPUT_DIR, LEVEL, PATCH_SIZE, OVERLAP) ```
+
+This command will create an HDF5 file in ```OUTPUT_DIR``` with a group corresponding to each WSI's name.  Each of these groups will have a dataset that holds all of the patches and associated overlap metadata which will be used during the recombine.
--- create_patches/patch.py
+++ create_patches/patch.py
@@ -3,8 +3,8 @@ import numpy as np
 import math
 from PIL import Image
 from tqdm import tqdm
-import datetime
 import shutil
+import h5py
 import time
 import os
 
@@ -55,28 +55,52 @@ def patch_image_and_mask(WSI_NAME, WSI_PATH, LEVEL, PATCH_SIZE):
 
 '''
 
+def patch_images_without_mask(INPUT_DIR, OUTPUT_DIR, LEVEL, PATCH_SIZE, overlap):
+
+    output_file = h5py.File(os.path.join(OUTPUT_DIR, "patches.hdf5"), "w")
+
+    for img_name in os.listdir(INPUT_DIR):
+        patch_without_mask(os.path.join(INPUT_DIR, img_name), output_file, LEVEL, PATCH_SIZE, overlap)
+
+    output_file.close()
+
+
+'''
+
+    This function creates patches of a wsi, and stores those patches (and metadata) 
+    in the following directory structure:
+
+            image patches:   ./create_patches/patches/image
+            metadata:        ./create_patches/patches
+
+'''
+
 
 def patch_image_without_mask(WSI_NAME, WSI_PATH, LEVEL, PATCH_SIZE, overlap):
     now = time.time()
-    print(os.path.join(WSI_PATH, f'{WSI_NAME}.svs')) 
-    patched_image, metadata = patch_without_mask(os.path.join(WSI_PATH, f'{WSI_NAME}.svs'), LEVEL, PATCH_SIZE, overlap)
 
-    if not os.path.exists("create_patches/patches"):
-        os.makedirs("create_patches/patches")
+    if not os.path.exists(os.path.join("create_patches", "patches.hdf5")):
+        f = h5py.File(os.path.join("create_patches", "patches.hdf5"), "w")
+    else: 
+        f = h5py.File(os.path.join("create_patches", "patches.hdf5"), "r")
+
+    patch_without_mask(os.path.join(WSI_PATH, f'{WSI_NAME}.svs'), f, LEVEL, PATCH_SIZE, overlap)
+
 
     #If there is an existing folder for patches of some ID, remove everything (older patches)
-    if WSI_NAME in os.listdir("create_patches/patches"):
-        print(f'deleting {WSI_NAME}')
-        shutil.rmtree("create_patches/patches/{}/".format(WSI_NAME))
+    # if WSI_NAME in os.listdir("create_patches/patches"):
+    #     print(f'deleting {WSI_NAME}')
+    #     shutil.rmtree("create_patches/patches/{}/".format(WSI_NAME))
 
-    os.makedirs("create_patches/patches/{}".format(WSI_NAME))
-    os.makedirs("create_patches/patches/{}/image".format(WSI_NAME))
+    # os.makedirs("create_patches/patches/{}".format(WSI_NAME))
+    # os.makedirs("create_patches/patches/{}/image".format(WSI_NAME))
     
     for i in range(len(patched_image)):
         Image.fromarray(patched_image[i][:,:,:3]).save("create_patches/patches/{}/image/{}_{}.jpeg".format(WSI_NAME, WSI_NAME, i), QUALITY=QUALITY, subsampling=0)
     with open("create_patches/patches/{}/overlap.txt".format(WSI_NAME), 'w') as f:        
         f.write("\n".join([str(dim) for dim in metadata]))
     print("Duration: " + str(time.time() - now))
+
 
 '''
     Main patching logic function, patches image and mask
@@ -132,13 +156,21 @@ def patch_with_mask(image_name, image, patch_level, patch_size):
 '''
     Secondary patching logic function, only patches image (not mask)
 '''
-def patch_without_mask(image, patch_level, patch_size, overlap_level):        
+def patch_without_mask(image, output_file, patch_level, patch_size, overlap_level):     
+
+    #create group in hdf5 file to store patches
+    image_name = image.split(os.path.sep)[-1][:-4]
+    group = output_file.create_group(image_name)  
     
     im = op.OpenSlide(image)
 
-    #get x and y dimensions of input image at specified level
-    im_x_dim = im.level_dimensions[patch_level][0]
-    im_y_dim = im.level_dimensions[patch_level][1]
+    try: 
+        #get x and y dimensions of input image at specified level
+        im_x_dim = im.level_dimensions[patch_level][0]
+        im_y_dim = im.level_dimensions[patch_level][1]
+    except:
+        print(f"Level {patch_level} not available for image {image_name}.  Returning.")
+        return
 
     #calculate number of patches in x and y direction
     num_x_patchest = math.ceil(im_x_dim / patch_size) 
@@ -165,8 +197,8 @@ def patch_without_mask(image, patch_level, patch_size, overlap_level):
                 num_y_patches -= 1
                 overlap_y = math.ceil((patch_size * num_y_patches - im_y_dim)/ (num_y_patches - 1))
     
-    #This will be return tensor (4 at the end since openslide returns 4 channels)
-    patched_image = np.empty([num_x_patches * num_y_patches, patch_size, patch_size, 4], dtype=np.uint8)
+
+    patches = group.create_dataset("patches", (num_x_patches * num_y_patches, patch_size, patch_size, 4), "i8")
     
     patch_count = 0  #Used to keep track of patch location in patched_image array
     anchor = [0,0] #top left corner of patch
@@ -174,7 +206,7 @@ def patch_without_mask(image, patch_level, patch_size, overlap_level):
     for i in tqdm(range(num_y_patches)):
         for j in range(num_x_patches):
             #Read patch and assign it to array that will be returned
-            patched_image[patch_count] = np.array(im.read_region((*anchor,), patch_level, (patch_size, patch_size)))              
+            patches[patch_count] = np.array(im.read_region((*anchor,), patch_level, (patch_size, patch_size)))              
             patch_count += 1
             #shift reading frame to the right - im.level_downsamples used to upsample the shift amount since top left coord is based on level 0
             anchor[0] += round(im.level_downsamples[patch_level]) * (patch_size - overlap_x)
@@ -182,6 +214,12 @@ def patch_without_mask(image, patch_level, patch_size, overlap_level):
         anchor[1] += round(im.level_downsamples[patch_level]) * (patch_size - overlap_y)
         anchor[0] = 0
 
-    print("overlap_x: {}, overlap_y: {}, patched_image_size: {}, num_x_patches: {}, num_y_patches: {}".format(overlap_x, overlap_y, len(patched_image), num_x_patches, num_y_patches))
+    #Set metadata
+    patches.attrs["overlap_x"] = overlap_x
+    patches.attrs["overlap_y"] = overlap_y
+    patches.attrs["num_x_patches"] = num_x_patches
+    patches.attrs["num_y_patches"] = num_y_patches
+
+    print("overlap_x: {}, overlap_y: {}, patched_image_size: {}, num_x_patches: {}, num_y_patches: {}".format(overlap_x, overlap_y, len(patches), num_x_patches, num_y_patches))
         
-    return patched_image, [overlap_x, overlap_y, num_x_patches, num_y_patches] #, im.get_thumbnail(im.level_dimensions[3]) 
+    return
